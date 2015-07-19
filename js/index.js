0cef037644c3c9e67199f65478b68ba77324a075
Javier BG <abrew_javier@hotmail.com>
2015-07-18 21:25:02 -0500

    calendario agragado en inici.html

--- js/index.js
+++ js/index.js
@@ -1,13 +1,27 @@
 $(document).ready(function(){
-    $("#dinamic").load("inicio.html");
+    loadPage("#dinamic", "inicio.html");
     /*Eventos click en menu superior y menu derecho*/
     $(this).on("click", "#menu-top a, #menu-left a", function(e){
         e.preventDefault();
-        $("#dinamic").load($(this).attr("href"));
+        loadPage("#dinamic", $(this).attr("href"));
     });
     /*Eventos click en lista acreditación*/
     $(this).on("click", "#acreditacion a", function(e){
         e.preventDefault();
-        $("#tramites-dinamic").load($(this).attr("href"));
+        loadPage("#tramites-dinamic", $(this).attr("href"));
     })
 });
+
+/**
+ * [Carga el archivo especificado en el parámetro "url", en la etiqueta especificada en el parámetro "place" ]
+ * @param {[String]} place  [[Un id o clase donde cargar el archivo]]
+ * @param {[String]} page [[La ruta del archivo a cargar]]
+ */
+function loadPage(place, url){
+    $(place).load(url, function(){
+        $("#calendar").datepicker({
+            inline:true,
+            dateFormat:"dd-mm-yy"
+        });
+    });
+}
